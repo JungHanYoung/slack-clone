aea73c6b091cc82fcc6aca83019bef763ed54745
Hanyoung Jeong <8735132@gmail.com>
2018-04-30 22:37:53 +0900

    apollo server example https://www.apollographql.com/docs/apollo-server/servers/express.html

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress } from 'apollo-server-express';
+
+const app = express();
+
+// bodyParser is needed just for POST.
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
+
+app.listen(8080);
